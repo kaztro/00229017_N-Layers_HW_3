53ab240a6392281ce72c80908d19d23e24b0b525
kaztro <00229017@uca.edu.sv>
2020-04-27 14:10:22 -0600

    I just configured the Thymeleaf in MVC pj...

---
+++ src/test/java/com/uca/_NLayers_HW_3/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca._NLayers_HW_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

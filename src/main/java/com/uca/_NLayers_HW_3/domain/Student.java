c8f415f4a1c136997eeed04e755d1e0f6b71134d
kaztro <00229017@uca.edu.sv>
2020-04-27 15:53:47 -0600

    efe, no me sale xd

---
+++ src/main/java/com/uca/_NLayers_HW_3/domain/Student.java
@@ -0,0 +1,56 @@
+package com.uca._NLayers_HW_3.domain;
+
+public class Student {
+	private String nombres,
+				   apellidos,
+				   lNac,
+				   cole,
+				   fNac;
+	private int telF,
+				telM;
+	public String getNombres() {
+		return nombres;
+	}
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+	public String getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+	public String getlNac() {
+		return lNac;
+	}
+	public void setlNac(String lNac) {
+		this.lNac = lNac;
+	}
+	public String getCole() {
+		return cole;
+	}
+	public void setCole(String cole) {
+		this.cole = cole;
+	}
+	public String getfNac() {
+		return fNac;
+	}
+	public void setfNac(String fNac) {
+		this.fNac = fNac;
+	}
+	public int getTelF() {
+		return telF;
+	}
+	public void setTelF(int telF) {
+		this.telF = telF;
+	}
+	public int getTelM() {
+		return telM;
+	}
+	public void setTelM(int telM) {
+		this.telM = telM;
+	}
+	
+	
+	
+}
